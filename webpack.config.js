c909f60384ad9cc1690abf31b2db85bd736295b6
The Soo-Rae Hong <the.soorae@gmail.com>
2017-03-20 16:20:32 -0700

    modified folder structure to src and dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-  entry: "./entry.jsx",
-  output: {
-    filename: "./js/bundle.js"
-  },
-  module: {
 
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    './src/entry'
+  ],
+  module: {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
@@ -16,8 +18,18 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-map',
   resolve: {
     extensions: ["",".js", ".jsx" ]
-  }
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+
+  devtool: 'source-map'
 };
